66ab8970cb6aaf8dbf421f47f5d98d418445f000
Irfan Avianto <irfan@jk1.ebdesk.com>
2019-10-31 15:15:18 +0700

    Add materials for UKDW-2

---
+++ UKDW/2019od/captcha_util.py
@@ -0,0 +1,97 @@
+import os
+import sys
+import cv2
+import imutils
+
+def split_captcha (image):
+    letters = []
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Add some extra padding around the image
+    gray = cv2.copyMakeBorder(gray, 8, 8, 8, 8, cv2.BORDER_REPLICATE)
+
+    # threshold the image (convert it to pure black and white)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    # find the contours (continuous blobs of pixels) the image
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Hack for compatibility with different OpenCV versions
+    contours = contours[0] if imutils.is_cv2() else contours[1]
+
+    letter_image_regions = []
+
+    # Now we can loop through each of the four contours and extract the letter
+    # inside of each one
+    for contour in contours:
+        # Get the rectangle that contains the contour
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        # Compare the width and height of the contour to detect letters that
+        # are conjoined into one chunk
+        if w / h > 1.25:
+            # This contour is too wide to be a single letter!
+            # Split it in half into two letter regions!
+            half_width = int(w / 2)
+            letter_image_regions.append((x, y, half_width, h))
+            letter_image_regions.append((x + half_width, y, half_width, h))
+        else:
+            # This is a normal letter by itself
+            letter_image_regions.append((x, y, w, h))
+
+    # If we found more or less than 4 letters in the captcha, our letter extraction
+    # didn't work correcly. Skip the image instead of saving bad training data!
+    if len(letter_image_regions) != 4:
+        return None
+
+    # Sort the detected letter images based on the x coordinate to make sure
+    # we are processing them from left-to-right so we match the right image
+    # with the right letter
+    letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
+
+    # Save out each letter as a single image
+    for letter_bounding_box in letter_image_regions:
+        # Grab the coordinates of the letter in the image
+        x, y, w, h = letter_bounding_box
+
+        # Extract the letter from the original image with a 2-pixel margin around the edge
+        letters.append (gray[y - 2:y + h + 2, x - 2:x + w + 2])
+    
+    return letters
+        
+def resize_to_fit(image, width, height):
+    """
+    A helper function to resize an image to fit within a given size
+    :param image: image to resize
+    :param width: desired width in pixels
+    :param height: desired height in pixels
+    :return: the resized image
+    """
+
+    # grab the dimensions of the image, then initialize
+    # the padding values
+    (h, w) = image.shape[:2]
+
+    # if the width is greater than the height then resize along
+    # the width
+    if w > h:
+        image = imutils.resize(image, width=width)
+
+    # otherwise, the height is greater than the width so resize
+    # along the height
+    else:
+        image = imutils.resize(image, height=height)
+
+    # determine the padding values for the width and height to
+    # obtain the target dimensions
+    padW = int((width - image.shape[1]) / 2.0)
+    padH = int((height - image.shape[0]) / 2.0)
+
+    # pad the image then apply one more resizing to handle any
+    # rounding issues
+    image = cv2.copyMakeBorder(image, padH, padH, padW, padW,
+        cv2.BORDER_REPLICATE)
+    image = cv2.resize(image, (width, height))
+
+    # return the pre-processed image
+    return image
